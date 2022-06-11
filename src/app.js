5b9124300a9740bd123de6aedf008456643fbcb7
Juan Varon <juanvarong@gmail.com>
2022-06-11 01:12:15 -0500

    📝Update skills and experience section

--- src/app.js
+++ src/app.js
@@ -9,3 +9,23 @@ function scrollHeader(){
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+// Experience Tabs
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab =>{
+  tab.addEventListener('click', () =>{
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent =>{
+      tabContent.classList.remove('experience__active')
+    })
+    target.classList.add('experience__active')
+
+    tabs.forEach(tab =>{
+      tab.classList.remove('experience__active')
+    })
+    tab.classList.add('experience__active')
+  })
+})
